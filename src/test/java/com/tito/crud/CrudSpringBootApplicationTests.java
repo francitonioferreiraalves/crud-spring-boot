d9ffc6e8c1f3fe8d15c6070e270ebe9039a562f1
francitonioferreiraalves <francitonio@gmail.com>
2021-04-06 12:21:37 -0300

    Projeto inicial

---
+++ src/test/java/com/tito/crud/CrudSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tito.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
